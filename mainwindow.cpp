75a691354c61583bd6d76a79adb8b74b8ea09215
Dũng Cận <88014896+dungcan2k5@users.noreply.github.com>
2024-10-17 16:56:48 +0700

    Cập nhật giao diện

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     connect(ui->toolButtonConfig, &QPushButton::clicked, this, &MainWindow::onToolButtonConfigClicked);
+
+    // Chỉnh lại kích thước 2 cột tiêu đề dọc và ngang của thời khoá biểu
+    ui->tableWidget->horizontalHeader()->setFixedHeight(50);
+    ui->tableWidget->verticalHeader()->setFixedWidth(120);
 }
 
 MainWindow::~MainWindow()
@@ -21,24 +25,24 @@ void MainWindow::onToolButtonConfigClicked()
     if (CONFIG_WINDOW_OPEN == true)
     {
         // Tạo animation cho thuộc tính "minimumWidth" của configWindow
-        QPropertyAnimation *animation1 = new QPropertyAnimation(ui->configWindow, "minimumWidth");
-        animation1->setDuration(1000);                               // Thời gian animation (ms)
-        animation1->setStartValue(ui->configWindow->maximumWidth()); // Giá trị bắt đầu
-        animation1->setEndValue(0);                                  // Giá trị kết thúc
-        animation1->setEasingCurve(QEasingCurve::InOutQuart);
-        animation1->start(QAbstractAnimation::DeleteWhenStopped); // Tự động xóa khi kết thúc
+        QPropertyAnimation *animation = new QPropertyAnimation(ui->configWindow, "minimumWidth");
+        animation->setDuration(500);                                // Thời gian animation (ms)
+        animation->setStartValue(ui->configWindow->maximumWidth()); // Giá trị bắt đầu
+        animation->setEndValue(0);                                  // Giá trị kết thúc
+        animation->setEasingCurve(QEasingCurve::InOutSine);
+        animation->start(QAbstractAnimation::DeleteWhenStopped); // Tự động xóa khi kết thúc
 
         CONFIG_WINDOW_OPEN = false;
     }
     else
     {
         // Tạo animation cho thuộc tính "minimumWidth" của configWindow
-        QPropertyAnimation *animation1 = new QPropertyAnimation(ui->configWindow, "minimumWidth");
-        animation1->setDuration(1000);                               // Thời gian animation (ms)
-        animation1->setStartValue(0); // Giá trị bắt đầu
-        animation1->setEndValue(ui->configWindow->maximumWidth());                                  // Giá trị kết thúc
-        animation1->setEasingCurve(QEasingCurve::InOutQuart);
-        animation1->start(QAbstractAnimation::DeleteWhenStopped); // Tự động xóa khi kết thúc
+        QPropertyAnimation *animation = new QPropertyAnimation(ui->configWindow, "minimumWidth");
+        animation->setDuration(500);                              // Thời gian animation (ms)
+        animation->setStartValue(0);                              // Giá trị bắt đầu
+        animation->setEndValue(ui->configWindow->maximumWidth()); // Giá trị kết thúc
+        animation->setEasingCurve(QEasingCurve::InOutSine);
+        animation->start(QAbstractAnimation::DeleteWhenStopped); // Tự động xóa khi kết thúc
 
         CONFIG_WINDOW_OPEN = true;
     }
